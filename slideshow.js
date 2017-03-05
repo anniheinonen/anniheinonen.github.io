074f3feeef8ced4d7118c4d53c3ceecf6104761e
anniheinonen <anni.heinonen@aalto.fi>
2017-03-05 22:05:38 +0200

    Add files via upload

---
+++ slideshow.js
@@ -0,0 +1,63 @@
+var index = 0;
+window.onload = function () {
+    'use strict';
+    $.getJSON("https://heinona10.firebaseio.com/.json", function (data) {
+        console.log(data);
+        index = parseInt(localStorage.getItem('indeksi'))
+        display();
+    });
+};
+
+function display() {
+    'use strict';
+    $.getJSON("https://heinona10.firebaseio.com/.json", function (data) {
+        $('#otsikko').hide().html(data.uutiset[index].otsikko).fadeIn(2000)
+        $('#paivamaara').hide().html(data.uutiset[index].paivamaara).fadeIn(2000);
+        $('#sisalto').fadeOut(1000).hide().html(data.uutiset[index].sisalto).fadeIn(2000);
+        localStorage.setItem("indeksi", index)
+    });
+}
+
+function nextSlide() {
+    'use strict';
+    if (index < 2) {
+        index += 1;
+        display();
+    } else {
+        index = 0;
+        display();
+    }
+}
+
+function previousSlide() {
+    'use strict';
+    if (index > 0) {
+        index -= 1;
+        display();
+    } else {
+        index = 2;
+        display();
+    }
+}
+
+var isPaused = false;
+var myVar = setInterval(function () { nextSlide() }, 7000);
+function playpause() {
+    'use strict';
+    if (!isPaused) {    
+        window.clearInterval(myVar);
+        isPaused = true;
+    } else {
+        myVar = setInterval(function () { nextSlide() }, 7000);
+        isPaused = false;
+    }
+}
+
+function toggleText(button_id) {
+    'use strict'
+    if (isPaused) {
+        document.getElementById(button_id).textContent = "play";
+    } else {
+        document.getElementById(button_id).textContent = "pause";
+    }
+}
